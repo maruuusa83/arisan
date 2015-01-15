5f0a2b9d9acb8df0d7e4d74f4eac47e3d4c75fb5
Daichi Teruya <tmarusa83@gmail.com>
2015-01-15 05:01:00 +0000

    Makefile was modified

---
+++ ifa_sys/ifa_sys.cpp
@@ -0,0 +1,48 @@
+/********************************************************************************
+ * Copyright (C) 2014 Daichi Teruya (@maruuusa83)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License.
+ *
+ * This program is destributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *******************************************************************************/
+#include <iostream>
+
+#include "common.h"
+#include "InterfaceAppAPI.h"
+
+#include "CmcAdapter.h"
+
+using std::cout;
+using std::endl;
+
+using marusa::swms::InterfaceAppAPI;
+using marusa::swms::JOB_ID;
+
+class MyIFAListener : public InterfaceAppAPI::IFACallbackListener
+{
+public:
+	void onRecvJobId(const InterfaceAppAPI::IFAContext &context,
+					 const JOB_ID &job_id){
+		cout << "MyIFAListener::onRecvJobId : job_id = " << job_id << endl;
+	}
+};
+
+
+int main()
+{
+	MyIFAListener *listener = new MyIFAListener();
+
+	//InterfaceAppAPI ifa(listener, cmc);
+
+	return (0);
+}
