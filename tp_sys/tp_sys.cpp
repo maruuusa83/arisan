d9c3363920d5bf11dc77c1d16bef47385b8deb44
Daichi Teruya <tmarusa83@gmail.com>
2017-05-30 03:44:06 +0000

    remove debug_print

--- tp_sys/tp_sys.cpp
+++ tp_sys/tp_sys.cpp
@@ -184,18 +184,6 @@ class MyTPListener : public TaskProcessorAPI::TPCallbackListener
         PRINT_BYTES(obtained_key); printf("\n");
         printf("\n**********\n\n");
 
-        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
-        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
-        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
-        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
-        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
-        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
-        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
-        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
-        printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
-
-        exit(1);
-
 	    return (trip_count);
 	}
 };
